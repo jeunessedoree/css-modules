68b39ee5d0ed1a7f434a11e736272086a1fbcd97
jdb <jdeburce@Jeunesses-MacBook-Pro.local>
2022-06-01 21:26:07 +0800

    Add more files

--- css-modules-demo/src/App.js
+++ css-modules-demo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Container from './Container';
+import CSSSpecificityContainer from './CSSSpecificityContainer';
 
 function App() {
   return (
     <div className="App">
       <Header title="I am a CSS module title" />
       <Container />
+      <CSSSpecificityContainer />
     </div>
   );
 }
---
+++ css-modules-demo/src/CSSSpecificityContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CSSSpecificityContainer.css';
+
+const CSSSpecificityContainer = () => {
+
+    return (
+        <div className="container" id="title">
+           I am a CSS Specificity Container
+        </div>
+    )
+}
+
+export default CSSSpecificityContainer;
